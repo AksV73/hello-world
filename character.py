9c86f79461b151461f0cf4298495c8c995393264
AksV73 <61354801+AksV73@users.noreply.github.com>
2020-03-03 19:32:13 +0530

    Vowel check
    To check if entered character is a vowel or not.

---
+++ character.py
@@ -0,0 +1,10 @@
+def charc(n):
+    m = n.lower()
+    if m=='a' or m=='e' or m=='i' or m=='o' or m=='u':
+        print("vowel")
+    else:
+        print("not vowel")
+
+
+n = input("enter:")
+charc(n)            
